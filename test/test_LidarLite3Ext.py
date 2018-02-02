09e5f310c0b9a0f7da5df2d0b4c870c56700b96f
Z223I <BruceWilsonPH@yahoo.com.ph>
2018-02-02 11:14:32 -0600

    Establishing pyunit tests.

--- test/test_LidarLite3Ext.py
+++ test/test_LidarLite3Ext.py
@@ -10,57 +10,20 @@ import unittest
 import time
 
 import sys
-sys.path.append('/home/pi/pythondev/RangeBot/RangeBot')
-from RangeBot import RangeBot
+sys.path.append('/home/pi/pythondev/LidarLite3Ext/LidarLite3Ext')
+from LidarLite3Ext import LidarLite3Ext
 
-class TestRangeBot(unittest.TestCase):
-    CHANNEL = 4
+class TestLidarLite3Ext(unittest.TestCase):
 
     def setUp(self):
-        self.test_range_bot = RangeBot(TestRangeBot.CHANNEL)
+        self.test_lidar_lite_3_ext = LidarLite3Ext()
 
     def tearDown(self):
         pass
 
     def test___init___(self):
-        self.assertEqual(self.test_range_bot.clip_distance, 10)
 
-    def test_set_clip_distance(self):
-        val = 6
-        self.test_range_bot.set_clip_distance(val)
-        self.assertEqual(self.test_range_bot.clip_distance, val)
-
-    def test_scan(self):
-        pass
-
-    def test_find_target2(self):
-        pass
-
-    def test_execute(self):
-        pass
-
-    def test_valid_hunt_A(self):
-        hits = 7
-        ranges = [100,100,100,100,100,100,100, \
-            24, 24, 24, 24, 24, 24, 24, \
-            100,100,100,100,100,100,100]
-
-        valid = self.test_range_bot.valid_hunt(ranges, hits)
-        self.assertTrue(valid)
-
-    def test_valid_hunt_B(self):
-        # Four hits is below the threshold for a valid hunt.
-        hits = 4
-
-        # The values of ranges is irrelavent.
-        ranges = [100,100,100,100,100,100,100, \
-            24, 24, 24, 24, 24, 24, 24, \
-            100,100,100,100,100,100,100]
-
-        valid = self.test_range_bot.valid_hunt(ranges, hits)
-        self.assertFalse(valid)
-
-    def test_execute_hunt(self):
+    def test_(self):
         pass
 
 
